4aa7983dbf373f19ce1a4ce093546f6271e0fc4a
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-02-19 22:19:25 +0530

    Added Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import AllEmployees from './components/Employee/AllEmployees';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Navbar />
+      <AllEmployees />
+    </>
+    
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" className={classes.title}>
+            Akshat Singhal App
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
---
+++ src/components/Employee/AllEmployees.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import { Table, TableRow, TableCell,TableBody,TableHead } from '@material-ui/core';
+import Axios from 'axios';
+
+class AllEmployees extends Component
+{
+
+
+    state = {
+        employees : []
+    }
+
+    componentDidMount(){
+        const URL = "https://akkiapp.herokuapp.com/employee/viewall";
+        Axios.get(URL).then(response => response.data)
+        .then((data)=>{
+            this.setState({employees : data});
+            console.log(this.state.employees);
+        })
+    }
+
+    render(){
+        return(
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell style={{fontWeight:"bold"}} component="th" color="inherit"> ID</TableCell>
+                        <TableCell style={{fontWeight:"bold"}} >First Name</TableCell>
+                        <TableCell style={{fontWeight:"bold"}}>Last Name</TableCell>
+                        <TableCell style={{fontWeight:"bold"}}>Address</TableCell>
+                        <TableCell style={{fontWeight:"bold"}}>Phone</TableCell>
+                        <TableCell style={{fontWeight:"bold"}}>Email</TableCell>
+                        <TableCell style={{fontWeight:"bold"}}>Date of Birth</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        this.state.employees.map(row =>
+                            (
+                                <TableRow key={row.id}>
+                                    <TableCell>{row.id}</TableCell>
+                                    <TableCell>{row.firstname}</TableCell>
+                                    <TableCell>{row.lastname}</TableCell>
+                                    <TableCell>{row.address}</TableCell>
+                                    <TableCell>{row.phone}</TableCell>
+                                    <TableCell>{row.email}</TableCell>
+                                    <TableCell>{row.date_of_birth}</TableCell>
+                                </TableRow>
+                            ))
+                    }
+                </TableBody>
+            </Table>
+        )
+    }
+}
+
+export default AllEmployees;
