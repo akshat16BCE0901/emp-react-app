6afbc1bd12150b9bc1ddb61e6e9db3a0df04358c
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-02-25 10:00:14 +0530

    Modified Jira view Module

--- src/components/Jira/CardSection.js
+++ src/components/Jira/CardSection.js
@@ -2,9 +2,24 @@ import React, {Component} from 'react';
 import CardColumns from 'react-bootstrap/CardColumns';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Axios from 'axios';
 
 class CardSection extends Component
 {
+
+    state =  {
+        allJiras : []
+    };
+
+    componentDidMount = () =>{
+        const URL = "https://akkiapp.herokuapp.com/jira/viewall";
+        Axios.get(URL).then(response => response.data)
+        .then((data)=>{
+            this.setState({allJiras : data});
+            console.log(this.state.allJiras);
+        }) 
+    }
+
     render(){
         return(
             <div className="row">
@@ -21,7 +36,24 @@ class CardSection extends Component
                             <Button variant="primary">Open</Button>
                         </Card.Footer>
                     </Card>
+                    {
+                        this.state.allJiras.map(row =>(
+                            <Card key={row.id}  border="primary" style={{ width: '18rem' }}>
+                                <Card.Header className="text-center h4">TPM - {row.id}</Card.Header>
+                                <Card.Body>
+                                    <Card.Title className="text-center">{row.title}</Card.Title>
+                                    <Card.Text className="text-center text-justify">
+                                       {row.description}
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer className="text-center">
+                                    <Button variant="primary">Open</Button>
+                                </Card.Footer>
+                        </Card>
+                        ))
+                    }
                 </CardColumns>
+                
             </div>
         )
     }
