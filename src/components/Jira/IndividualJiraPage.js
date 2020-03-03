49598ea7b98477d95901fa4ba5696dd38ed1793d
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-03-03 22:44:36 +0530

    Added Individual Jira Display Module

---
+++ src/components/Jira/IndividualJiraPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class IndividualJiraPage extends Component
+{
+
+    componentDidMount = () =>{
+
+    }
+    render(){
+       return(
+        <div className="row" style={{backgroundColor:"white",padding : "10px"}}>
+            <div className="col-md-12">
+
+            </div>
+        </div>
+       )
+    }
+}
